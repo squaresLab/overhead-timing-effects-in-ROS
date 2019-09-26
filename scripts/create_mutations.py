b8a9330b4ff3344f3107a665371ca0a83e4de304
DebyKatz <dskatz@cs.cmu.edu>
2019-09-26 13:30:36 -0400

    adding some toy scripts and patches

---
+++ scripts/create_mutations.py
@@ -0,0 +1,53 @@
+import argparse
+from comby import Comby
+import difflib
+import sys
+
+# This script takes in a set of files and generates source code
+# mutations according to the chosen strategy
+
+# also binary mutations?
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--returns', action="store_true", default=False,
+                        help="Add instrumentation point before " +
+                        "each return statement")
+    parser.add_argument('--files', action='append', type=str,
+                        help="Specify the files to instrument")
+    args = parser.parse_args()
+    return args
+
+
+def mutate_returns(files):
+    comby = Comby()
+    match = "return :[x];"
+    rewrite = "{sleep 0.5; return :[x];}"
+    for fn in files:
+        with open(fn) as old:
+            source_old = old.read()
+            source_new = comby.rewrite(source_old, match, rewrite)
+            fn_new = "%s.new" % fn
+            with open(fn_new, 'w') as file_new:
+                file_new.write(source_new)
+        with open(fn) as old:
+            fromlines = old.readlines()
+        with open(fn_new) as new:
+            tolines = new.readlines()
+
+        diff = difflib.unified_diff(fromlines, tolines, fn, fn)
+
+        diff_fn = "%s.diff" % fn
+        with open(diff_fn, 'w') as diff_file:
+            diff_file.writelines(diff)
+
+def main():
+    args = parse_args()
+
+    if args.returns and args.files:
+        mutations = mutate_returns(args.files)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/play_with_roswire.py
@@ -0,0 +1,132 @@
+import argparse
+from enum import Enum
+from ruamel.yaml import YAML
+
+import roswire
+
+
+class Mutation:
+
+    def __init__(self, base_version, diff):
+        self.base_version = base_version
+        self.diff = diff
+
+    def from_dict(d):
+        diff = d['diff']
+        base_version = BaseVersion(d['base-version'])
+        return Mutation(base_version, diff)
+
+
+class MutationDatabase:
+    def __init__(self, contents):
+        self.__contents = contents
+
+    def from_file(fn):
+        with open(fn, 'r') as f:
+            yml = YAML().load(f)
+        contents = [Mutation.from_dict(dd) for dd in yml]
+        return MutationDatabase(contents)
+
+    def __iter__(self):
+        yield from self.__contents
+
+
+class BaseVersion(Enum):
+    Copter_3_6_7 = 'Copter-3.6.7'
+    Copter_3_6_6 = 'Copter-3.6.6'
+    Copter_3_6_5 = 'Copter-3.6.5'
+    Copter_3_6_4 = 'Copter-3.6.4'
+
+    def image(self):
+        return 'dskatz/ardu:{}'.format(self.value)
+
+    def snapshot(self):
+        name = 'dsk-ardu:{}'.format(self.value)
+        coverage_instructions = BugZooCoverageInstructions.from_dict({
+            'type': 'gcov',
+            'files-to-instrument': [
+                'APMrover2/APMrover2.cpp',
+                'ArduCopter/ArduCopter.cpp',
+                'ArduPlane/ArduPlane.cpp'
+            ]
+        })
+        snapshot = BugZooSnapshot(
+            name=name,
+            image=self.image,
+            dataset='dsk-ardu',
+            program='ArduPilot',
+            source=None,
+            source_dir='/opt/ardupilot',
+            languages=[bugzoo.Language.C],
+            tests=bugzoo.core.TestSuite([]),
+            compiler=bugzoo.compiler.WafCompiler(300),
+            instructions_coverage=coverage_instructions)
+        return snapshot
+
+
+def get_diff_fn(args):
+    return "../mutations.yml"
+
+
+def get_docker_image(args):
+    return "roswire/example:mavros"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    rsw = roswire.ROSWire()
+
+    # Read a diff file (generate or existing)
+    diff_fn = get_diff_fn(args)
+
+    # Find the docker image
+    docker_image = get_docker_image(args)
+
+    # apply the diff file to the docker container
+    try:
+        mutation_database = MutationDatabase.from_file(diff_fn)
+    except:
+        mutation = Mutation(base_version, diff)
+
+    for mutation in mutation_database:
+        diff = mutation.diff
+
+        print("\n\nlaunching docker image: %s" % docker_image)
+
+        with rsw.launch(docker_image) as system:
+            context = '/ros_ws/src/ArduPilot'
+
+            print("printing diff")
+            print(diff)
+
+            print("printing context")
+            print(context)
+
+            print("applying patch")
+            system.files.patch(context, diff)
+            print("patch applied")
+
+            print("rebuilding...")
+            dir_workspace = '/ros_ws'
+
+            catkin = system.catkin(dir_workspace)
+            catkin.build()
+            print("rebuilt")
+
+    # rebuild the docker container
+    # run tests via service calls (inputs, with and without instrumentation
+    # extract logs via rosbag
+
+    # analyze bag files
+    pass
+
+
+if __name__ == '__main__':
+    main()
