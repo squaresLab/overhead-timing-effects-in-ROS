1db93b8a064e075c187f0b599acabc002d9fb0b4
DebyKatz <dskatz@cs.cmu.edu>
2020-01-09 12:44:07 -0500

    modifying create_mutations to support parameter sweep

--- scripts/create_mutations.py
+++ scripts/create_mutations.py
@@ -11,22 +11,29 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--returns', action="store_true", default=False,
                         help="Add instrumentation point before " +
-                        "each return statement")
+                        "return statements")
     parser.add_argument('--files', action='append', type=str,
                         help="Specify the files to instrument")
+    parser.add_argument('--delay', type=float, default=0.5,
+                        help="Delay(secs) to insert at each insertion point.")
+    parser.add_argument('--weight', type=float, default=0.5,
+                        help="Portion of identified locations to instrument (between 0.0 and 1.0)")
     args = parser.parse_args()
     return args
 
 
-def mutate_returns(files):
+def mutate_files(files, returns=False, delay=0.5):
     comby = Comby()
-    match = "return :[x];"
-    rewrite = "{sleep 0.5; return :[x];}"
+    if returns:
+        match = "return :[x];"
+        rewrite = "{sleep %f; return :[x];}" % delay
+    else:
+        raise NotImplemented
     for fn in files:
         with open(fn) as old:
             source_old = old.read()
             source_new = comby.rewrite(source_old, match, rewrite)
-            fn_new = "%s.new" % fn
+            fn_new = "%s_%f.new" % (fn, delay)
             with open(fn_new, 'w') as file_new:
                 file_new.write(source_new)
         with open(fn) as old:
@@ -44,8 +51,8 @@ def mutate_returns(files):
 def main():
     args = parse_args()
 
-    if args.returns and args.files:
-        mutations = mutate_returns(args.files)
+    if args.files:
+        mutations = mutate_files(args.files, returns=args.returns)
 
 
 if __name__ == '__main__':
