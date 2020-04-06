3b4021bf3b7ee55f0d9cc3e2ef15f3d25e5c803a
DebyKatz <dskatz@cs.cmu.edu>
2020-04-06 11:27:00 -0400

    analysis and graphing of ardupilot logs

--- scripts/graph_trajectory.py
+++ scripts/graph_trajectory.py
@@ -1,10 +1,12 @@
 import argparse
+import datetime
 import logging
 import sys
-from typing import Tuple
+from typing import Dict, List, Tuple
 
 import matplotlib.pyplot as plt
 import matplotlib.colors
+from matplotlib.ticker import ScalarFormatter
 import numpy as np
 #np.set_printoptions(threshold=sys.maxsize)
 
@@ -16,11 +18,13 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--log_db", type=str)
     parser.add_argument("--log_fn", type=str, action="append")
     parser.add_argument("--logging", type=str, default="graph.log")
+    parser.add_argument("-i", "--individual", action="store_true",
+                        default=False)
     args = parser.parse_args()
     return args
 
 
-def graph_one_log(log: np.array, fn: str = "FIG.png") -> None:
+def graph_one_log(log: np.array, fn: str = "FIG.png", title: str = "None") -> None:
     fig, ax = plt.subplots()
     ax.ticklabel_format(useOffset=False)
 
@@ -28,11 +32,13 @@ def graph_one_log(log: np.array, fn: str = "FIG.png") -> None:
     #lat = log[:,1][1:]
     #lon = log[:,2][1:]
 
-    logging.debug(f"lat: {lat}")
-    logging.debug(f"lon: {lon}")
+    #logging.debug(f"lat: {lat}")
+    #logging.debug(f"lon: {lon}")
     ax.scatter(lat, lon, c=time_elapsed, s=(relative_alt/100))
     ax.set_xlabel("Latitude")
     ax.set_ylabel("Longitude")
+    if title != "None":
+        ax.set_title(title)
     logging.debug(f"saving to filename: {fn}")
     fig.savefig(fn)
     plt.close(fig)
@@ -54,8 +60,8 @@ def next_color(in_color: Tuple[float, float, float]) -> Tuple[float, float, floa
         next_color = (in_color[0] + 0.05, in_color[1], in_color[2])
     else:
         next_color = in_color
-    # TODO: the less than equivalents
-    logging.debug(f"next_color: {next_color}")
+
+    #logging.debug(f"next_color: {next_color}")
     return next_color
 
 
@@ -65,8 +71,11 @@ def animate_logs(logs) -> None:
     plt.close(fig)
 
 
-def extract_series(log) -> Tuple[np.array, np.array, np.array, np.array, np.array]:
-    log = np.array([x for x in log if (x[1] != 0 and x[2] != 0)])
+def extract_series(log, discard_zero: bool=True) -> Tuple[np.array, np.array, np.array, np.array, np.array]:
+    if discard_zero:
+        log = np.array([x for x in log if (x[1] != 0 and x[2] != 0)])
+    else:
+        log = np.array([x for x in log])
 
     lat = log[:,1]
     lon = log[:,2]
@@ -82,7 +91,77 @@ def extract_series(log) -> Tuple[np.array, np.array, np.array, np.array, np.arra
     return (lat, lon, time_elapsed, alt, relative_alt)
 
 
-def graph_logs(logs) -> None:
+def get_delay_weight(mutation_fn: str) -> Tuple[float, float]:
+    logging.debug(f"mutation_fn: {mutation_fn}")
+
+    if mutation_fn.endswith(".diff"):
+        mutation_fn = mutation_fn[:-5]
+
+    split = mutation_fn.split("_")
+
+    logging.debug(f"split: {split}")
+    for section in split:
+        logging.debug("section: {section}")
+        if section.startswith("d") or section.startswith("w"):
+            try:
+                value = float(section[1:])
+            except ValueError:
+                logging.debug(f"{section[1:]} is not a float")
+                continue
+            logging.debug(f"value: {value}")
+            if section.startswith("d"):
+                delay = value
+            elif section.startswith("w"):
+                weight = value
+            else:
+                assert (False), "Incompatible if logic."
+
+    return delay, weight
+
+
+def get_total_time(log: np.array, use_external_time: bool=True) -> float:
+    if not use_external_time:
+        time_elapsed = log[:,0]
+        total_time_ms = time_elapsed[-1]
+        total_time = total_time_ms / 1000
+    else:
+        timestamps = log[:,-1]
+        timestamp_initial = datetime.datetime.fromtimestamp(timestamps[1])
+        timestamp_final = datetime.datetime.fromtimestamp(timestamps[-1])
+        total_time = (timestamp_final - timestamp_initial).total_seconds()
+    return total_time
+
+
+def graph_time(logs: Dict[str, List[Tuple[str, str, np.array]]],
+               use_external_time: bool=True) -> None:
+    delays = []
+    weights = []
+    times = []
+    for subset_label, logs_subset in logs.items():
+        for log_fn, mutation_fn, log in logs_subset:
+            if mutation_fn == "None" or mutation_fn == None:
+                continue
+            delay, weight = get_delay_weight(mutation_fn)
+            delays.append(delay)
+            weights.append(weight)
+            total_time = get_total_time(log, use_external_time)
+            times.append(total_time)
+
+    fig, ax = plt.subplots()
+    ax.ticklabel_format(useOffset=False)
+    ax.set_xscale("log")
+    ax.xaxis.set_major_formatter(ScalarFormatter())
+    ax.set_xlabel("Delay amount (seconds, log scale)")
+    ax.set_ylabel("Delay probability")
+    ax.set_title("Total running time of ArduCopter")
+    #ax.ticklabel_format(style="plain")
+    scatter = ax.scatter(delays, weights, c=times, cmap="Oranges")
+    cbar = fig.colorbar(scatter)
+    cbar.ax.set_ylabel("total time elapsed (s)")
+    fig.savefig("TOTAL_TIME.png")
+
+
+def graph_logs(logs: Dict[str, List[Tuple[str, str, np.array]]]) -> None:
 
     fig, ax = plt.subplots()
     ax.ticklabel_format(useOffset=False)
@@ -95,9 +174,9 @@ def graph_logs(logs) -> None:
         # Define the label
         # TODO
         #logging.debug(f"logs_subset[0]: {logs_subset[0]}")
-        logging.debug(f"type(logs_subset[0]): {type(logs_subset[0])}")
+        #logging.debug(f"type(logs_subset[0]): {type(logs_subset[0])}")
         # Plot each log in a variation in the color family
-        for log_fn, log in logs_subset:
+        for log_fn, mutation_fn, log in logs_subset:
             lat, lon, time_elapsed, alt, relative_alt = extract_series(log)
             ax.scatter(lat, lon, c=time_elapsed, s=(relative_alt/100))
             # logging.debug(f"color: {color}")
@@ -107,6 +186,8 @@ def graph_logs(logs) -> None:
             color = next_color(color)
 
     fig.savefig("MANY_FIG.png")
+
+    graph_time(logs)
 
 
 def main() -> None:
@@ -123,13 +204,14 @@ def main() -> None:
 
     # logging.debug(f"logs: {logs}")
 
-    for logs_subset in logs.values():
-        filename_counter = 1
-        for log_fn, log in logs_subset:
-            log_fn_short = log_fn.split("/")[-1].split(".")[0]
-            filename = f"ONE_LOG_{log_fn_short}.png"
-            graph_one_log(log, fn=filename)
-            filename_counter = filename_counter + 1
+    if (args.individual):
+        for logs_subset in logs.values():
+            filename_counter = 1
+            for log_fn, mutation_fn, log in logs_subset:
+                log_fn_short = log_fn.split("/")[-1].split(".")[0]
+                filename = f"ONE_LOG_{log_fn_short}.png"
+                graph_one_log(log, fn=filename, title=mutation_fn)
+                filename_counter = filename_counter + 1
 
     graph_logs(logs)
 
--- scripts/log_analysis.py
+++ scripts/log_analysis.py
@@ -22,18 +22,19 @@ def access_bag_db(db_fn: str) -> Tuple[sqlite3.Cursor, sqlite3.Connection]:
     return cursor, conn
 
 
-def get_fns_from_rows(cursor: sqlite3.Cursor, log_dir: str) -> List[str]:
+def get_fns_from_rows(cursor: sqlite3.Cursor, log_dir: str) -> List[Tuple[str, str]]:
     rows = cursor.fetchall()
     log_fns = []
     for row in rows:
         # logging.debug(row)
         log_fn = os.path.join(log_dir, f"{row[0]}.tlog")
+        mutation_fn = row[7]
         # logging.debug(log_fn)
-        log_fns.append(log_fn)
+        log_fns.append((log_fn, mutation_fn))
     return log_fns
 
 
-def get_from_db(log_db: str, log_dir: str = "../bags") -> Dict[str, List[str]]:
+def get_from_db(log_db: str, log_dir: str = "../bags") -> Dict[str, List[Tuple[str,str]]]:
     log_fns: Dict[str, List[np.array]] = dict()
     cursor, conn = access_bag_db(log_db)
 
@@ -79,26 +80,43 @@ def get_json(log_fn: str) -> List[Dict]:
     return json_data
 
 
-def convert_logs(log_fns: List[str]) -> List[Tuple[str, np.array]]:
-    global_pos_lists = []
+def convert_logs(log_fns: List[Tuple[str, str]],
+                 field_type: str="GLOBAL_POSITION_INT") -> List[Tuple[str, str, np.array]]:
+    one_field_lists = []
     fn_count = 0
     total_fns = len(log_fns)
     logging.debug(f"convert_logs log_fns: {log_fns}")
-    for log_fn in log_fns:
+    for log_fn, mutation_fn in log_fns:
         logging.debug(f"convert_logs log_fn: {log_fn}")
         # convert the tlog to json, if it's not already json
         log_json = get_json(log_fn)
-        global_pos = [x["data"] for x in log_json if
-                      x["meta"]["type"] == "GLOBAL_POSITION_INT"]
-        global_pos_list = ([(x['time_boot_ms'], x['lat'], x['lon'],
-                             x['alt'], x['relative_alt'], x['vx'],
-                             x['vy'], x['vz'], x['hdg']) for x in
-                            global_pos])
-        global_np = np.array(global_pos_list)
-        global_pos_lists.append((log_fn, global_np))
+
+        one_field_dict = [x["data"] for x in log_json if
+                          x["meta"]["type"] == field_type]
+        timestamp_list = [x["meta"]["timestamp"] for x in log_json if
+                          x["meta"]["type"] == field_type]
+        assert(len(one_field_dict) == len(timestamp_list))
+
+        if field_type == "GLOBAL_POSITION_INT":
+            one_field_list = ([(x[0]['time_boot_ms'], x[0]['lat'], x[0]['lon'],
+                                 x[0]['alt'], x[0]['relative_alt'], x[0]['vx'],
+                                 x[0]['vy'], x[0]['vz'], x[0]['hdg'], x[1])
+                                for x in zip(one_field_dict, timestamp_list)])
+
+        elif field_type == "MISSION_ITEM_REACHED":
+            one_field_list = ([(x[0]['seq'], x[1])
+                               for x in zip(one_field_dict, timestamp_list)])
+        elif field_type == "MISSION_CURRENT":
+            one_field_list = ([(x[0]['seq'], x[1])
+                               for x in zip(one_field_dict, timestamp_list)])
+        else:
+            logging.error(f"Field: {field_type} not supported")
+            raise NotImplementedError
+        one_field_np = np.array(one_field_list)
+        one_field_lists.append((log_fn, mutation_fn, one_field_np))
         fn_count += 1
         logging.debug(f"File {fn_count} of {total_fns}")
-    return global_pos_lists
+    return one_field_lists
 
 
 def equalize_lengths(logs: List[np.array], length: int = 0) -> List[np.array]:
@@ -116,7 +134,7 @@ def equalize_lengths(logs: List[np.array], length: int = 0) -> List[np.array]:
     return new_logs
 
 
-def logs_to_np(log_fns: Dict[str, List[str]]) -> Dict[str, List[Tuple[str, Any]]]:
+def logs_to_np(log_fns: Dict[str, List[Tuple[str, str]]]) -> Dict[str, List[Tuple[str, str, Any]]]:
     all_logs = dict()
     # Turn the log files into np.arrays of the data
     for label, log_fns_subset in log_fns.items():
@@ -128,27 +146,27 @@ def logs_to_np(log_fns: Dict[str, List[str]]) -> Dict[str, List[Tuple[str, Any]]
     return all_logs
 
 
-def to_json_ready(all_logs: Dict[str, List[Tuple[str, np.array]]]) -> Dict[str, List[Tuple[str, List]]]:
+def to_json_ready(all_logs: Dict[str, List[Tuple[str, str, np.array]]]) -> Dict[str, List[Tuple[str, str, List]]]:
     json_ready = dict()
     for key in all_logs:
         json_ready_list = []
         for item in all_logs[key]:
-            json_ready_list.append((item[0], item[1].tolist()))
+            json_ready_list.append((item[0], item[1], item[2].tolist()))
         json_ready[key] = json_ready_list
     return json_ready
 
 
-def json_to_np(json_logs: Dict[str, List[Tuple[str, List]]]) -> Dict[str, List[Tuple[str, np.array]]]:
+def json_to_np(json_logs: Dict[str, List[Tuple[str, str, List]]]) -> Dict[str, List[Tuple[str, str, np.array]]]:
     all_logs = dict()
     for key in json_logs:
         all_logs_list = []
         for item in json_logs[key]:
-            all_logs_list.append((item[0], np.array(item[1])))
+            all_logs_list.append((item[0], item[1], np.array(item[2])))
         all_logs[key] = all_logs_list
     return all_logs
 
 
-def memoize_log_db(log_db: str) -> Dict[str, List[Tuple[str, np.array]]]:
+def memoize_log_db(log_db: str) -> Dict[str, List[Tuple[str, str, np.array]]]:
     date = time.strftime("%m-%d-%Y")
     memoized_fn = f"{log_db}_{date}.json"
     logging.debug(memoized_fn)
@@ -166,17 +184,19 @@ def memoize_log_db(log_db: str) -> Dict[str, List[Tuple[str, np.array]]]:
     return all_logs
 
 
-def get_logs(args: argparse.Namespace) -> Dict[str, List[Tuple[str, np.array]]]:
-    log_fns: Dict[str, List[str]] = dict()
+def get_logs(args: argparse.Namespace) -> Dict[str, List[Tuple[str, str, np.array]]]:
+    log_fns: Dict[str, List[Tuple[str, str]]] = dict()
 
     logging.debug(f"args.log_fn: {args.log_fn}")
 
     # if args.log_fn and len(args.log_fn) > 1:
     if args.log_fn and len(args.log_fn) > 0:
-        log_fns = insert(log_fns, "manual", args.log_fn)
+        log_fns = insert(log_fns, "manual", [(x, "None") for x in args.log_fn] )
         logging.debug(f"log_fns: {log_fns}")
         all_logs = logs_to_np(log_fns)
     elif args.log_db:
+        #TODO: This assumes all the logs in the database have the same
+        # mission, which isn't always a good assumption. Fix it.
         all_logs = memoize_log_db(args.log_db)
 
     return all_logs
